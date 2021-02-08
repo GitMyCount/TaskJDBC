4dee88aeaef884b5d4e0aad8900d61b6db875e6d
user <dvd.off@mail.ru>
2021-02-09 01:21:29 +0700

    add commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserService us = new UserServiceImpl();
+
+        us.createUsersTable();
+
+        us.saveUser("Petr", "Petrov", (byte) 30);
+        us.saveUser("Jora", "Dobrov", (byte) 40);
+        us.saveUser("Vera", "Sidorova", (byte) 20);
+        us.saveUser("Lara", "Popova", (byte) 25);
+
+        us.removeUserById(4);
+
+        System.out.println(us.getAllUsers());
+
+        us.cleanUsersTable();
+
+        us.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    String user = "root";
+    String password = "root";
+    String url = "jdbc:mysql://localhost:3306/bzdnusers";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection connection = null;
+
+    public Connection getConnection() throws ClassNotFoundException {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            return connection;
+        }
+        return connection;
+    }
 }
