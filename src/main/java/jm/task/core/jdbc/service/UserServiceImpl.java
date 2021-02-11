14b410dba77301f8f928e87efb5e5dc53866038f
user <dvd.off@mail.ru>
2021-02-11 23:28:30 +0700

    add commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,117 +1,39 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    Connection conn = null;
-    Statement stat = null;
-    PreparedStatement ps = null;
-    Util util = new Util();
+    UserDao ud = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-            stat.executeUpdate("CREATE TABLE IF NOT EXISTS User " +
-                    "(id INTEGER AUTO_INCREMENT, " +
-                    " name VARCHAR(50), " +
-                    " lastName VARCHAR (50), " +
-                    " age INTEGER not NULL, " +
-                    " PRIMARY KEY (id))");
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ud.createUsersTable();
+
     }
 
     public void dropUsersTable() {
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-            stat.executeUpdate("DROP TABLE IF EXISTS User");
-
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ud.dropUsersTable();
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-
-            ps = conn.prepareStatement("INSERT INTO User(name ,lastName, age) VALUES(?, ?, ?)");
-            ps.setString(1,name);
-            ps.setString(2,lastName);
-            ps.setByte(3,age);
-            ps.execute();
-
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ud.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-            stat.executeUpdate("DELETE FROM User WHERE id = " + id);
-
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ud.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-            ResultSet resSet = stat.executeQuery("select * from bzdnusers.User");
-
-            while (resSet.next()) {
-                User user = new User();
-                user.setId(resSet.getLong("id"));
-                user.setName(resSet.getString("name"));
-                user.setLastName(resSet.getString("lastName"));
-                user.setAge(resSet.getByte("age"));
-                list.add(user);
-            }
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return ud.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try {
-            conn = util.getConnection();
-            stat = conn.createStatement();
-            stat.executeUpdate("Truncate table User");
-
-            stat.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ud.cleanUsersTable();
 
     }
 }
